2c4960a865b124c95834873f0d50c200f321fad0
Cherry Microbe <skmoquin@gmail.com>
2026-02-17 19:33:06 -0500

    Add other half of path

--- Scripts/linecalc.gd
+++ Scripts/linecalc.gd
@@ -4,6 +4,7 @@ extends Line2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var big_string: String = ""
-	for i in range(self.points.size()-1, -1, -1):
-		big_string += str(1152 - self.points[i].x) + ", " + str(self.points[i].y) + ", "
+	for i in range(self.points.size()):
+		var j: int = self.points.size() - (i + 1)
+		big_string += str(1152 - self.points[j].x) + ", " + str(self.points[j].y) + ", "
 	print(big_string)
